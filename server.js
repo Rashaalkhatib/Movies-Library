c8b6dded583433fc8100cbc7246269f1b3ed8fb7
Eng-Rasha AL-Khatib <86823412+Rashaalkhatib@users.noreply.github.com>
2022-02-22 15:12:44 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,117 @@
+'use strict';
+const express = require('express');
+const movies = require("data.json");
+const axios = require("axios");
+const pg = require("pg");
+
+const app = express();
+
+
+const DATABASE_URL = process.'postgres://rasha:318020@localhost:5432/movies';
+const client = new pg.Client(DATABASE_URL);
+
+
+function movie(id, title, release_date, poster_path, overview) {
+    this.id= id;
+    this.title = title;
+    this.release_date = release_date;
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
+
+
+app.get('/', handler);
+app.get('/trending', trindingHandler);
+app.get('/search', searchHandller);
+app.get(errorHandller);
+app.post("/addMovie", addMovie);
+app.get("/getMovies", getMovieHandler);
+
+function handler(req, res) {
+    let arr = [];
+    let onemovie = new movie(value.id, value.title, value.release_date, value.poster_path, value.overview)
+    arr.push(onemovie);
+    }
+        return res.status(200).json(movies.data);
+}
+
+
+
+
+
+function trindingHandler(req, res) {
+    let arr = [];
+    axios.get('https://api.themoviedb.org/3/trending/all/day?1531f1a558c8357ce8990cf887ff196e8f5402ec=<<1531f1a558c8357ce8990cf887ff196e8f5402ec>>')
+        .then(apiResponse => {
+            apiResponse.data.movies.map(value => {
+                let onemovie = new movie(value.id, value.title, value.release_date, value.poster_path, value.overview)
+                arr.push(onemovie);
+            })
+            return res.status(200).json(arr);
+        }).catch (error=> {
+        errorHandller(error, req, res);
+    
+    })
+   
+};
+
+
+
+
+
+function errorHandller(error, req, res) {
+    const err = {
+        status =500
+        massage=error
+    }
+    return res.status(500.send("not found");
+}
+
+
+
+function searchHandller(req, res) {
+    let arr = [];
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=668baa4bb128a32b82fe0c15b21dd699&language=en-US&query=The&page=2')
+        .then(apiResponse => {
+            apiResponse.data.movies.map(value => {
+                let onemovie = new movie(value.id, value.title, value.release_date, value.poster_path, value.overview)
+                arr.push(onemovie);
+            })
+            return res.status(200).json(arr);
+        }).catch(error => {
+            errorHandller(error, req, res);
+
+        })
+
+}
+
+
+
+
+function addMovie(req, res) {
+    const addmov = req.body;
+    
+    const sql = 'INSERT INTO movie (title,release_date ,poster_path ,overview ) VALUES($1, $2, $3, $4, $5,) RETURNING *';
+    const values = [addMov.title,addmov.title,addmov.release_date,addmov.poster_path,addmov.overview]
+    client.query(sql, values).then((result) => {
+        return res.status(201).json(result.rows);
+    }).catch((error) => {
+        errorHandler(error, req, res);
+    });
+};
+
+function getMovieHandler(req, res) {
+    const sql = 'SELECT * FROM movie';
+
+    client.query(sql).then((result) => {
+        return res.status(200).json(result.rows);
+    }).catch((error) => {
+        errorHandler(error, req, res);
+    });
+};
+
+
+
+app.listen(3000, () => {
+    console.log("Listen on 3000")
+})
